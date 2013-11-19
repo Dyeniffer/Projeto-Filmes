ac2020977f7b77bfbf6fc0aa0219eef226c8c043
paulolimaesilva <paulo.limaesilva09@gmail.com>
2013-11-19 11:44:44 -0200

    commit dia 19/11 (2)

--- sistema/Filme/src/gui/Action/SalvarBKPAction.java
+++ sistema/Filme/src/gui/Action/SalvarBKPAction.java
@@ -7,13 +7,14 @@ import java.awt.Panel;
 import java.awt.event.ActionEvent;
 
 import javax.swing.AbstractAction;
+import javax.swing.JFileChooser;
 import javax.swing.JOptionPane;
 import javax.swing.JTree;
 
 import pojo.Filme;
 import dao.FilmeDao;
 
-public class EscolherLocalDoBKPAction extends AbstractAction {
+public class SalvarBKPAction extends AbstractAction {
 
 
 	private static final long serialVersionUID = 1L;
@@ -21,19 +22,15 @@ public class EscolherLocalDoBKPAction extends AbstractAction {
 
 	private CriarBKPBDPanel panel;
 
-	public EscolherLocalDoBKPAction(CriarBKPBDPanel panel) {
+	public SalvarBKPAction(CriarBKPBDPanel panel) {
 		super("Escolher local para salvar BKP");
 		this.panel = panel;
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
-		Panel panel = new Panel();
-		JTree arvore = new JTree();
-		panel.add(arvore);
 		
-		arvore.
-		
+		JFileChooser url = new JFileChooser();
 		
 	}
 }
--- sistema/Filme/src/gui/CriarBKPBDPanel.java
+++ sistema/Filme/src/gui/CriarBKPBDPanel.java
@@ -1,6 +1,6 @@
 package gui;
 
-import gui.Action.EscolherLocalDoBKPAction;
+import gui.Action.SalvarBKPAction;
 
 import javax.swing.JPanel;
 import java.awt.Color;
@@ -22,9 +22,15 @@ import java.io.FileOutputStream;
 import java.util.zip.Deflater;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import javax.swing.BoxLayout;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
 
 public class CriarBKPBDPanel extends JPanel {
-	private final Action action_1 = new SwingAction_1();
 	private String caminho;
 	byte[] buffer = new byte[1024];
 	
@@ -33,90 +39,29 @@ public class CriarBKPBDPanel extends JPanel {
 	 */
 	public CriarBKPBDPanel() {
 		setBackground(Color.WHITE);
-		setLayout(new FormLayout(new ColumnSpec[] {
-				FormFactory.RELATED_GAP_COLSPEC,
-				FormFactory.DEFAULT_COLSPEC,
-				FormFactory.RELATED_GAP_COLSPEC,
-				FormFactory.DEFAULT_COLSPEC,
-				FormFactory.RELATED_GAP_COLSPEC,
-				FormFactory.DEFAULT_COLSPEC,
-				FormFactory.RELATED_GAP_COLSPEC,
-				FormFactory.DEFAULT_COLSPEC,
-				FormFactory.RELATED_GAP_COLSPEC,
-				FormFactory.DEFAULT_COLSPEC,
-				FormFactory.RELATED_GAP_COLSPEC,
-				FormFactory.DEFAULT_COLSPEC,
-				FormFactory.RELATED_GAP_COLSPEC,
-				FormFactory.DEFAULT_COLSPEC,
-				FormFactory.RELATED_GAP_COLSPEC,
-				FormFactory.DEFAULT_COLSPEC,
-				FormFactory.RELATED_GAP_COLSPEC,
-				FormFactory.DEFAULT_COLSPEC,
-				FormFactory.RELATED_GAP_COLSPEC,
-				FormFactory.DEFAULT_COLSPEC,},
-			new RowSpec[] {
-				FormFactory.RELATED_GAP_ROWSPEC,
-				FormFactory.DEFAULT_ROWSPEC,
-				FormFactory.RELATED_GAP_ROWSPEC,
-				FormFactory.DEFAULT_ROWSPEC,
-				FormFactory.RELATED_GAP_ROWSPEC,
-				FormFactory.DEFAULT_ROWSPEC,
-				FormFactory.RELATED_GAP_ROWSPEC,
-				FormFactory.DEFAULT_ROWSPEC,}));
 		
-		JButton btnLocal = new JButton(new EscolherLocalDoBKPAction(this));
-		btnLocal.setToolTipText("Cadastrar filme");
-		btnLocal.setMnemonic(KeyEvent.VK_ENTER);
-		btnLocal.setFont(new Font("Segoe UI Light", Font.PLAIN, 14));
-		add(btnLocal, "17, 16, 15, 1");
+		JButton btnCriarCpiaDe = new JButton(new SalvarBKPAction(this));
+		btnCriarCpiaDe.setFont(new Font("Segoe UI Light", Font.PLAIN, 13));
+		GroupLayout groupLayout = new GroupLayout(this);
+		groupLayout.setHorizontalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGroup(groupLayout.createSequentialGroup()
+					.addGap(125)
+					.addComponent(btnCriarCpiaDe)
+					.addContainerGap(158, Short.MAX_VALUE))
+		);
+		groupLayout.setVerticalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGroup(groupLayout.createSequentialGroup()
+					.addGap(132)
+					.addComponent(btnCriarCpiaDe)
+					.addContainerGap(141, Short.MAX_VALUE))
+		);
+		setLayout(groupLayout);
 		
-		JButton btnCriarCpiaDe = new JButton("criar c\u00F3pia de seguran\u00E7a");
-		btnCriarCpiaDe.setAction(action_1);
-		add(btnCriarCpiaDe, "20, 8");
-
-	}
 
 	
-	private class SwingAction_1 extends AbstractAction {
-		public SwingAction_1() {
-			putValue(NAME, "Criar Cópia de Segurança");
-			putValue(SHORT_DESCRIPTION, "Some short description");
-		}
-		public void actionPerformed(ActionEvent e) {
-			String localDoBD = "c:/filmes/";
-			
-			try {
-				 
-				// cria o arquivo zip
-				ZipOutputStream saidaDeStream = new ZipOutputStream(new FileOutputStream(caminho));
-				 
-				// marca o modo de compreensão do arquivo
-				saidaDeStream.setLevel(Deflater.BEST_COMPRESSION);
-				 
-				// carrega o arquivo em um stream
-				FileInputStream entradaDeStream = new FileInputStream(localDoBD);
-				 
-				// cria uma entrada no zip para o arquivo
-				saidaDeStream.putNextEntry(new ZipEntry(localDoBD));
-				 
-				// transfere os dados do arquivo para o zip
-				saidaDeStream.write(buffer);
-				
-				 
-				// fecha a entrada do arquivo no zip
-				saidaDeStream.closeEntry();
-				 
-				// fecha o arquivo
-				entradaDeStream.close();
-				
-				saidaDeStream.finish();
-				
-				// fecha o arquivo zip
-				saidaDeStream.close();
-				 
-				} catch (Exception e1) {
-					System.out.println("bkp não realizado");
-				}
-		}
+		
+
 	}
 }
